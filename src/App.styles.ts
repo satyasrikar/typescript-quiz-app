7e45884426a15b59aad022a32f4ece3a9d4f4f78
Satyasri Kar <satya.kar98@gmail.com>
2021-06-14 14:58:27 +0530

    Card styling and utils

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -37,8 +37,8 @@ export const Wrapper = styled.div`
   }
 
   h1 {
-    font-family: Fascinate Inline;
-    background-image: linear-gradient(180deg, #fff, #87f1ff);
+    font-family: Big Shoulders Inline Display;
+    background-image: linear-gradient(180deg, #fff, #fff);
     font-weight: 400;
     background-size: 100%;
     background-clip: text;
@@ -46,7 +46,7 @@ export const Wrapper = styled.div`
     -webkit-text-fill-color: transparent;
     -moz-background-clip: text;
     -moz-text-fill-color: transparent;
-    filter: drop-shadow(2px 2px #0085a3);
+    // filter: drop-shadow(2px 2px #0085a3);
     font-size: 70px;
     text-align: center;
     margin: 20px;
@@ -55,13 +55,15 @@ export const Wrapper = styled.div`
   .start,
   .next {
     cursor: pointer;
-    background: linear-gradient(180deg, #ffffff, #ffcc91);
-    border: 2px solid #d38558;
+    font-size: 1rem;
+    // background: linear-gradient(180deg, #ffffff, rgba(0, 0, 57, 0.73));
+    border: 2px solid white;
     box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
     border-radius: 10px;
-    height: 40px;
+    height: 50px;
     margin: 20px 0;
     padding: 0 40px;
+    color: black;
   }
 
   .start {
